ceb93c653e6e9e42d4373c5c8c28498359a6d47d
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-02-13 21:17:37 +0300

    Initial commit

---
+++ src/test/java/ru/clevertec/newsmanagment/SystemNewsManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.newsmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemNewsManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
