7e5269bae24a9a5b9d74eb4a57cfecd96c01f968
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-02-15 02:08:08 +0300

    add news controller edit security config

--- src/main/java/ru/clevertec/newsmanagement/config/SecurityConfig.java
+++ src/main/java/ru/clevertec/newsmanagement/config/SecurityConfig.java
@@ -5,13 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.clevertec.newsmanagement.entity.Role;
 import ru.clevertec.newsmanagement.security.JwtAuthenticationFilter;
 
 @Configuration
@@ -27,6 +27,12 @@ public class SecurityConfig {
         http
                 .csrf()
                 .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/api/v1/news/**").hasAnyRole(Role.ADMIN.name(),Role.JOURNALIST.name())
+                .requestMatchers("/api/v1/comment/**").hasAnyRole()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
---
+++ src/main/java/ru/clevertec/newsmanagement/controller/NewsController.java
@@ -0,0 +1,12 @@
+package ru.clevertec.newsmanagement.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/news")
+@RequiredArgsConstructor
+public class NewsController {
+
+}
