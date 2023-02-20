9ad6c91cb53b6952b6ca875e887214cb6653eebc
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-02-20 21:34:32 +0300

    add jwt validator test

--- src/test/java/ru/clevertec/newsmanagement/validator/JwtValidatorTest.java
+++ src/test/java/ru/clevertec/newsmanagement/validator/JwtValidatorTest.java
@@ -1,33 +1,84 @@
 package ru.clevertec.newsmanagement.validator;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
 class JwtValidatorTest {
+    @Mock
+    private Authentication authentication;
 
     @BeforeEach
     void setUp() {
+        SecurityContextHolder.clearContext();
     }
-
-    @AfterEach
-    void tearDown() {
+    @Test
+    void isHeaderBearerExistWithNullShouldReturnsFalse() {
+        assertThat(JwtValidator.isHeaderBearerExist(null)).isFalse();
     }
 
     @Test
-    void isHeaderBearerExist() {
-
+    void isHeaderBearerExistWithNonBearerHeaderShouldReturnsFalse() {
+        String header = "Token abcdefg123456";
+        assertThat(JwtValidator.isHeaderBearerExist(header)).isFalse();
     }
 
     @Test
-    void isUsernameExist() {
+    void isHeaderBearerExistWithBearerHeaderShouldReturnsTrue() {
+        String header = "Bearer abcdefg123456";
+        assertThat(JwtValidator.isHeaderBearerExist(header)).isTrue();
     }
 
     @Test
-    void isSecurityAuthenticationEmpty() {
+    void isUsernameExistWithNullUsernameShouldReturnsFalse() {
+        assertThat(JwtValidator.isUsernameExist(null)).isFalse();
     }
 
     @Test
-    void isSecurityContextEmpty() {
+    void isUsernameExistWithEmptyAuthenticationShouldReturnsTrue() {
+        String username = "testuser";
+        assertThat(JwtValidator.isUsernameExist(username)).isTrue();
+    }
+
+    @Test
+    void isUsernameExistWithNonEmptyAuthenticationShouldReturnsFalse() {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String username = "testuser";
+        assertThat(JwtValidator.isUsernameExist(username)).isFalse();
+    }
+
+    @Test
+    void isSecurityAuthenticationEmptyWithNullSecurityContextShouldReturnsTrue() {
+        assertThat(JwtValidator.isSecurityAuthenticationEmpty()).isTrue();
+    }
+
+    @Test
+    void isSecurityAuthenticationEmptyWithEmptySecurityContextShouldReturnsTrue() {
+        assertThat(JwtValidator.isSecurityAuthenticationEmpty()).isTrue();
+    }
+
+    @Test
+    void isSecurityAuthenticationEmptyWithNonEmptyContextAndNullAuthShouldReturnsTrue() {
+        SecurityContextHolder.getContext().setAuthentication(null);
+        assertThat(JwtValidator.isSecurityAuthenticationEmpty()).isTrue();
+    }
+
+    @Test
+    void isSecurityAuthenticationEmptyWithNonEmptySecurityContextAndNonEmptyAuthenticationShouldReturnsFalse() {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        assertThat(JwtValidator.isSecurityAuthenticationEmpty()).isFalse();
+    }
+
+    @Test
+    void isSecurityContextEmptyWithNonEmptySecurityContextShouldReturnsFalse() {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        assertThat(JwtValidator.isSecurityContextEmpty()).isFalse();
     }
 }
