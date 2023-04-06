11ddfb86efb07003970f925a48a52be452a5a861
Lenovo Laptop <ishankulievdayantch@yandex.ru>
2023-04-06 22:14:19 +0300

    add eureka server add config server add news-service(only in deploy stage) add user-service(empty)

---
+++ news-service/src/main/java/ru/clevertec/newsmanagement/newsservice/controller/TestController.java
@@ -0,0 +1,12 @@
+package ru.clevertec.newsmanagement.newsservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String test() {
+        return "Test message";
+    }
+}
